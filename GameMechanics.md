da8238626c6d6fe013eaf20685bff126660eb68f
BAYMAX\Felix <felix@playgen.com>
2018-07-03 16:12:45 +0100

    update tutorial namings and descriptions

--- GameMechanics.md
+++ GameMechanics.md
@@ -20,13 +20,30 @@ Where the characters skills are defined and any limitations (eg. is there 1 main
 ### <span style="color:red">**TODO**</span> Character Dialogue
 
 ### <span style="color:red">**TODO**</span> Enforced Team Management Restrictions
-Max team size, talk time, Number of practice sessions, season length. Links to be able to make changes to these.
+Max team size, talk time, Number of practice sessions, season length. Links to be able to make   changes to these.
 
 ### <span style="color:red">**TODO**</span> Post-Race Standings
 Describe how the post race standings are calculated, and show example code
 
 ### <span style="color:red">**TODO**</span> Tutorial States
-How the tutorial is designed/created, how each step is defined and how to add/remove a step
+How the tutorial is designed/created, how each step is defined and how to add/remove a step, included details of spreadsheet.
+
+Each step in the tutorial are defined by the following variables
+
+Variable | Type | Description
+:--- | :--- | :---
+SectionName | string | Section Name is only used in inspector to identify each step easily
+SectionTextHolder | List\<LanguageKeyValuePair> | Text to be displayed to the user, contains text for all lanuages
+ShowOnLeft | bool | If the popup should be shown on the left side of the screen
+HighlightedObject | List\<string> | Path to the object that will be highlighted to guide the player (from Canvas level)
+Triggers | List\<TriggerKeyValuePair> | The events that must happen for the player to complete this step of the tutorial
+UniqueEvents | bool | Whether the events that happen before must be unique and in the correct order
+EventTRiggerCountRequired | int | The number of times players must complete activate the trigger event to continue to the next step
+SafeToSave | bool | If the current step in the tutorial can be saved, sometimes UI must be open for the current step to activate/finish properly
+BlacklistButtons | List\<StringList> | Buttons which cannot be interacted with at this stage in the tutorial
+CustomAttributes | List\<string> | Additional attributes to help set up the stage in the tutorial
+
+The current tutorial steps are defined in the [SportsTeamManagerTutorial Excel File](stm-unity\Assets\Resources\Tutorial\SportsTeamManagerTutorial.xlsx), which is converted to JSON and converted to a list of TutorialObjects using the [Context Menu](https://unity3d.com/sites/default/files/styles/original/public/learn/MenuItems06.png?itok=NZTNMINK) for TutorialController. 
 
 ### <span style="color:red">**TODO**</span> Inter-team and Manager Relationships
 Brief overview of the way in which crew memebers relationships with other crew and the manager affect their performance and how this is calculated, link to FAtiMA
--- stm-unity/Assets/Scripts/TutorialObject.cs
+++ stm-unity/Assets/Scripts/TutorialObject.cs
@@ -32,14 +32,15 @@ public class TriggerKeyValuePair
 [Serializable]
 public class StringList
 {
+	[TextArea]
     public List<string> List;
 
-    public StringList(List<string> l)
+	public StringList(List<string> l)
     {
         List = l;
     }
 
-    public StringList(string[] l)
+	public StringList(string[] l)
     {
         List = l.ToList();
     }
@@ -52,17 +53,45 @@ public class TutorialObject
 	/// Section Name is only used in inspector to identify each step easily
 	/// </summary>
 	public string SectionName; 
+	/// <summary>
+	/// Text to be displayed to the user
+	/// </summary>
     public List<LanguageKeyValuePair> SectionTextHolder;
-    public bool Reversed;
-    public List<StringList> HighlightedObjects;
-    public List<TriggerKeyValuePair> Triggers;
-    public bool UniqueEvents;
-    public int EventTriggerCountRequired;
-    public int SaveNextSection;
-    public List<StringList> BlacklistButtons;
-    public List<string> CustomAttributes;
+	/// <summary>
+	/// If the popup should be shown on the left side of the screen
+	/// </summary>
+	public bool ShowOnLeft;
+	/// <summary>
+	/// Path to the object that will be highlighted to guide the player (from Canvas level)
+	/// </summary>
+	[TextArea]
+	public List<string> HighlightedObject;
+	/// <summary>
+	/// The events that must happen for the player to complete this step of the tutorial
+	/// </summary>
+	public List<TriggerKeyValuePair> Triggers;
+	/// <summary>
+	/// Whether the events that happen before must be unique and in the correct order
+	/// </summary>
+	public bool UniqueEvents;
+	/// <summary>
+	/// The number of times players must complete activate the trigger event to continue to the next step
+	/// </summary>
+	public int EventTriggerCountRequired;
+	/// <summary>
+	/// If the current step in the tutorial can be saved, sometimes UI must be open for the current step to activate/finish properly
+	/// </summary>
+	public bool SafeToSave;
+	/// <summary>
+	/// Buttons which cannot be interacted with at this stage in the tutorial
+	/// </summary>
+	public List<StringList> BlacklistButtons;
+	/// <summary>
+	/// Additional attributes to help set up the stage in the tutorial
+	/// </summary>
+	public List<string> CustomAttributes;
 
-    public TutorialObject(Dictionary<string, List<string>> text, List<string[]> highlightObjs, bool reversed, KeyValuePair<string, string>[] triggers, int triggerCount, bool uniqueTriggers, int saveSection, List<List<string>> blacklist, List<string> attributes, string sectionName)
+    public TutorialObject(Dictionary<string, List<string>> text, string highlightObjs, bool showOnLeft, KeyValuePair<string, string>[] triggers, int triggerCount, bool uniqueTriggers, bool safeToSave, List<List<string>> blacklist, List<string> attributes, string sectionName)
     {
 	    SectionName = sectionName;
         SectionTextHolder = new List<LanguageKeyValuePair>();
@@ -75,15 +104,11 @@ public class TutorialObject
         {
             Triggers.Add(new TriggerKeyValuePair(kvp.Key, kvp.Value));
         }
-        HighlightedObjects = new List<StringList>();
-        foreach (var obj in highlightObjs)
-        {
-            HighlightedObjects.Add(new StringList(obj));
-        }
-        Reversed = reversed;
+	    HighlightedObject = new List<string> {highlightObjs};
+        ShowOnLeft = showOnLeft;
         EventTriggerCountRequired = triggerCount;
         UniqueEvents = uniqueTriggers;
-        SaveNextSection = saveSection;
+        SafeToSave = safeToSave;
         BlacklistButtons = new List<StringList>();
         foreach (var obj in blacklist)
         {
